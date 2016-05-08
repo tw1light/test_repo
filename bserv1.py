8b599446ea6922add2b3a022e7323e019ce1d0e4
Aleksandr Bolshelapov <bolshelapov@gmail.com>
2016-05-08 17:57:03 +0300

    Create bserv1.py
    new test file

---
+++ bserv1.py
@@ -0,0 +1,15 @@
+import socket
+import os
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.1', 2222))
+s.listen(10)
+while True:
+  for i in range(10):
+    npid = os.fork()
+  conn, addr = s.accept()
+  while True:
+    data = conn.recv(1024)
+    if not data: break
+    if data == b'close(): break
+    conn.send(data)
+  conn.close()
